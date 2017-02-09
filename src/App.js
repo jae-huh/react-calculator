005938d6a9308ca6fcfb8b00d740c3a7fc48daeb
Jae Huh <jaeeunhuh@gmail.com>
2017-02-09 22:04:26 +1300

    Make first react components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hello from './Hello'
+import Button from './Button'
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h1>Welcome to React</h1>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Hello name="Jae" />
+        <Hello name="Rowan" />
+        <Button>New Btn</Button>
       </div>
     );
   }
---
+++ src/Button.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Button.css';
+
+
+class Button extends React.Component {
+  render() {
+    var text = this.props.children
+    console.log(this.props)
+    return (
+      <button className="Button">{text}</button>
+    );
+  }
+}
+
+export default Button;
