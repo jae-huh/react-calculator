fafc946b77dace30b50d9ed940e1216caa2400f1
Jae Huh <jaeeunhuh@gmail.com>
2017-02-10 21:12:54 +1300

    remove click handlers

--- src/Button.js
+++ src/Button.js
@@ -3,17 +3,12 @@ import './Button.css';
 
 
 class Button extends React.Component {
-  onClick = () => {
-    var text = this.props.children
-    this.props.onClick(text)
-  }
-
   render() {
     var text = this.props.children
 
     console.log(this.props)
     return (
-      <button className="Button" onClick={this.onClick}>{text}</button>
+      <button className="Button">{text}</button>
     );
   }
 }
--- src/Calculator.js
+++ src/Calculator.js
@@ -8,27 +8,20 @@ class Calculator extends React.Component {
     ans: 0
   }
 
-  updateAnswer = (num) => {
-    console.log('updateAnswer', num)
-    this.setState({
-      ans: num
-    })
-  }
-
   render() {
     var ans = this.state.ans
     return (
       <div className="Calculator">
         <Display ans={ans} />
-        <Button onClick={this.updateAnswer}>7</Button>
-        <Button onClick={this.updateAnswer}>8</Button>
-        <Button onClick={this.updateAnswer}>9</Button><br />
-        <Button onClick={this.updateAnswer}>4</Button>
-        <Button onClick={this.updateAnswer}>5</Button>
-        <Button onClick={this.updateAnswer}>6</Button><br />
-        <Button onClick={this.updateAnswer}>1</Button>
-        <Button onClick={this.updateAnswer}>2</Button>
-        <Button onClick={this.updateAnswer}>3</Button>
+        <Button>7</Button>
+        <Button>8</Button>
+        <Button>9</Button><br />
+        <Button>4</Button>
+        <Button>5</Button>
+        <Button>6</Button><br />
+        <Button>1</Button>
+        <Button>2</Button>
+        <Button>3</Button>
       </div>
     );
   }
